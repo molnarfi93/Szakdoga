f3faf443e1feb1cf13fad52923ccc2c7d87d4f92
molnarfi93 <55393260+molnarfi93@users.noreply.github.com>
2020-10-13 17:37:57 +0200

    Add files via upload

---
+++ klass.py
@@ -0,0 +1,12 @@
+class Class:
+    def __init__(self, name, grade, subject, type, weekly_periods, headcount, no):
+        self.name = name
+        self.grade = grade
+        self.subject = subject
+        self.type = type
+        self.weekly_periods = weekly_periods
+        self.sum_periods = 0
+        self.headcount = headcount
+        self.room = ""
+        self.teacher = ""
+        self.no = no
