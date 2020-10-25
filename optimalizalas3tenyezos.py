a3874c846354cdc28c3207d3463db1082d3d597f
molnarfi93 <55393260+molnarfi93@users.noreply.github.com>
2020-10-25 13:20:32 +0100

    Update optimalizalas3tenyezos.py

--- optimalizalas3tenyezos.py
+++ optimalizalas3tenyezos.py
@@ -1,27 +1,32 @@
 from optimalizalas2tenyezos import classes, NUM_CLASSES
 import data_loader
 
-teachers = data_loader.load_teachers(r'tanarok.csv')
+teachers = data_loader.load_teachers(r'C:\Users\Brendi\Documents\Szakdoga\tanarok.json')
 NUM_TEACHERS = len(teachers)
 
 pairings = [[] for _ in range(len(classes))]
 for i in range(NUM_CLASSES):
     for j in range(NUM_TEACHERS):
-        if (classes[i].subject == teachers[j].either_subject) or (classes[i].subject == teachers[j].other_subject):
-            pairings[i].append(1)
-        else:
-            pairings[i].append(0)
+        for k in range(len(teachers[j].subjects)):
+            if classes[i].subject == teachers[j].subjects[k]:
+                pairings[i].append(1)
+            else:
+                pairings[i].append(0)
+
+sum_periods = []
+for i in range(NUM_TEACHERS):
+    sum_periods.append(0)
 
 for i in range(NUM_CLASSES):
     for j in range(NUM_TEACHERS):
         boole = False
         if pairings[i][j] == 1:
             for k in range(j, NUM_TEACHERS):
-                if (teachers[k].sum_periods < teachers[j].sum_periods) & (pairings[i][k] == 1):
+                if (sum_periods[k] < sum_periods[j]) & (pairings[i][k] == 1):
                     boole = True
             if not boole:
                 classes[i].teacher = teachers[j].name
-                teachers[j].sum_periods += classes[i].weekly_periods
+                sum_periods[j] += classes[i].weekly_periods
                 break
 
 for i in range(NUM_CLASSES):
