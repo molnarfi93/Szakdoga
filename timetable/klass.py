f0fc5ec6c40dd1b0099c5e9bdd07e40c8090d3f2
Imre Piller <piller@iit.uni-miskolc.hu>
2020-10-28 14:10:57 +0100

    Renaming and some code modification

--- timetable/klass.py
+++ timetable/klass.py
@@ -1,18 +1,3 @@
-class Subject:
-
-    def __init__(self, name, type, weekly_periods):
-        self._name = name
-        self._type = type
-        self._weekly_periods = weekly_periods
-
-    @staticmethod
-    def create_subject_from_dict(name, d):
-        """
-        """
-        subject = Subject(name, d['type'], d['weekly_periods'])
-        return subject
-
-
 class Class:
 
     @staticmethod
--- timetable/optimalizalas3tenyezos.py
+++ timetable/optimalizalas3tenyezos.py
@@ -4,33 +4,29 @@ import data_loader
 teachers = data_loader.load_teachers(r'C:\Users\Brendi\Documents\Szakdoga\tanarok.json')
 NUM_TEACHERS = len(teachers)
 
-pairings = [[] for _ in range(len(classes))]
-for i in range(NUM_CLASSES):
-    for j in range(NUM_TEACHERS):
-        for k in range(len(teachers[j].subjects)):
-            if classes[i].subject == teachers[j].subjects[k]:
+pairings = [[] for _ in classes]
+for i, klass in enumerate(classes):
+    for teacher in teachers:
+	for subject in teacher.subjects:
+            if klass.subject == subject:
                 pairings[i].append(1)
             else:
                 pairings[i].append(0)
 
-sum_periods = []
-for i in range(NUM_TEACHERS):
-    sum_periods.append(0)
+sum_periods = [0] * len(teachers)
 
-for i in range(NUM_CLASSES):
-    for j in range(NUM_TEACHERS):
+for i, klass in enumerate(classes):
+    for j, teacher in enumerate(teacher):
+        # TODO: Rename the boole to a more verbose name!
         boole = False
         if pairings[i][j] == 1:
             for k in range(j, NUM_TEACHERS):
-                if (sum_periods[k] < sum_periods[j]) & (pairings[i][k] == 1):
+                if sum_periods[k] < sum_periods[j] and pairings[i][k] == 1:
                     boole = True
             if not boole:
-                classes[i].teacher = teachers[j].name
-                sum_periods[j] += classes[i].weekly_periods
+                klass.teacher = teacher.name
+                sum_periods[j] += klass.weekly_periods
                 break
 
 for i in range(NUM_CLASSES):
     print(classes[i].name, classes[i].subject, classes[i].teacher)
-
-
-
