e3c3b11e78c96660f7a6a66742e26ca3994fb0d1
molnarfi93 <55393260+molnarfi93@users.noreply.github.com>
2020-10-30 13:08:01 +0100

    Update optimalizalas4tenyezos.py

--- optimalizalas4tenyezos.py
+++ optimalizalas4tenyezos.py
@@ -1,38 +1,38 @@
-from optimalizalas3tenyezos import classes, NUM_CLASSES, NUM_TEACHERS
+from optimalizalas3tenyezos import groups, NUM_GROUPS, NUM_TEACHERS
 
 NUM_TIME_WINDOWS = 45
 MAX_PERIODS = NUM_TEACHERS
 
 
-def getTimetable():
-    timetable = []
-    seated_classes = []
-    for i in range(MAX_PERIODS):
-        for j in range(NUM_CLASSES):
-            if classes[j].sum_periods < classes[j].weekly_periods:
-                boole = True
-                for k in range(len(seated_classes)):
-                    if (classes[j].name == seated_classes[k].name) or (classes[j].grade == seated_classes[k].grade and classes[j].type != seated_classes[k].type):
-                        boole = False
+sum_periods = [0] * len(groups)
+def getPeriod():
+    period = []
+    seated_groups = []
+    for _ in range(MAX_PERIODS):
+        for group in range(NUM_GROUPS):
+            if sum_periods[group] < groups[group].weekly_periods:
+                ok = True
+                for seated_group in range(len(seated_groups)):
+                    if (groups[group].name == seated_groups[seated_group].name) or (groups[group].grade == seated_groups[seated_group].grade and groups[group].type != seated_groups[seated_group].type):
+                        ok = False
                         break
-                    elif classes[j].room == seated_classes[k].room:
-                        boole = False
+                    elif groups[group].room == seated_groups[seated_group].room:
+                        ok = False
                         break
-                    elif classes[j].teacher == seated_classes[k].teacher:
-                        boole = False
+                    elif groups[group].teacher == seated_groups[seated_group].teacher:
+                        ok = False
                         break
-                if boole:
-                    seated_classes.append(classes[j])
-                    classes[j].sum_periods += 1
-                    timetable.append(classes[j])
+                if ok:
+                    seated_groups.append(groups[group])
+                    sum_periods[group] += 1
+                    period.append(groups[group])
                     break
-    return timetable
+    return period
 
 
-timetables = []
-for i in range(NUM_TIME_WINDOWS):
-    print(i)
-    timetables.append(getTimetable())
-    for j in range(len(timetables[i])):
-        print(timetables[i][j].name, timetables[i][j].subject, timetables[i][j].teacher, timetables[i][j].room)
+periods = []
+for period in range(NUM_TIME_WINDOWS):
+    periods.append(getPeriod())
+    for assignment in range(len(periods[period])):
+        print(periods[period][assignment].name, periods[period][assignment].subject, periods[period][assignment].teacher, periods[period][assignment].room)
     print("************************************************")
