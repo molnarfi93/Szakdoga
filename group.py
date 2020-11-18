1a6acad434310e17c7b5b15b90fb4457f77f2cec
molnarfi93 <55393260+molnarfi93@users.noreply.github.com>
2020-11-18 14:43:03 +0100

    Add files via upload

---
+++ group.py
@@ -0,0 +1,12 @@
+class Group:
+    def __init__(self, name, grade, subject, type, weekly_periods, headcount):
+        self.name = name
+        self.grade = grade
+        self.subject = subject
+        self.type = type
+        self.weekly_periods = weekly_periods
+        self.room = ""
+        self.teacher = ""
+        self.headcount = headcount
+
+
