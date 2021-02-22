3fc22e293a5f7117a046330df06af9cf9d31a67d
molnarfi93 <55393260+molnarfi93@users.noreply.github.com>
2021-02-22 14:59:25 +0100

    Add files via upload

--- generator.py
+++ generator.py
@@ -2,71 +2,72 @@ import room_assignment
 import teacher_assignment
 import window_classification
 import genetic_algorithm
-from room import Room
-from teacher import Teacher
-from simple_group import SimpleGroup
 import exporter
 
 time_windows = genetic_algorithm.time_windows
 
 
-def generateTimetable(timetable_datas, rooms, teachers, simple_groups):
-    for room in range(len(rooms)):
-        rooms[room] = Room(rooms[room]['name'], rooms[room]['capacity'], rooms[room]['subjects'])
-    for teacher in range(len(teachers)):
-        teachers[teacher] = Teacher(teachers[teacher]['name'], teachers[teacher]['subjects'],
-                                    teachers[teacher]['balance'], teachers[teacher]['extremisms'],
-                                    teachers[teacher]['begin_time'], teachers[teacher]['end_time'])
-    for group in range(len(simple_groups)):
-        simple_groups[group] = SimpleGroup(simple_groups[group]['name'], simple_groups[group]['grade'],
-                                           simple_groups[group]['subjects'], simple_groups[group]['headcount'])
-
+def generateTimetables(rooms, teachers, simple_groups):
     groups = room_assignment.main(simple_groups, rooms)
-    if timetable_datas['add_manually'] is False:
-        groups = teacher_assignment.main(groups, teachers)
+    groups = teacher_assignment.main(groups, teachers)
     periods = window_classification.main(groups, teachers)
     periods = genetic_algorithm.main(teachers, periods)
 
     group_timetables = []
     for group in range(len(simple_groups)):
-        timetable = {'name': simple_groups[group].name, 'days': [], 'periods': [],
-                     'subjects': [], 'rooms': [], 'teachers': []}
+        timetable = {'name': simple_groups[group].name, 'days': ["" for _ in range(40)],
+                     'periods': ["" for _ in range(40)], 'subjects': ["" for _ in range(40)],
+                     'rooms': ["" for _ in range(40)], 'teachers': ["" for _ in range(40)]}
         for period in range(len(periods)):
-            for assignment in range(len(periods[period])):
+            for assignment in range(len(periods[period]['assignments'])):
                 if periods[period]['assignments'][assignment].name == simple_groups[group].name:
-                    timetable['days'].append(periods[period]['day'])
-                    timetable['periods'].append(periods[period]['period'])
-                    timetable['subjects'].append(periods[period]['assignments'][assignment].subject)
-                    timetable['rooms'].append(periods[period]['assignments'][assignment].room)
-                    timetable['teachers'].append(periods[period]['assignments'][assignment].teacher)
+                    timetable['days'][period] = periods[period]['day']
+                    timetable['periods'][period] = periods[period]['period']
+                    timetable['subjects'][period] = periods[period]['assignments'][assignment].subject
+                    timetable['rooms'][period] = periods[period]['assignments'][assignment].room
+                    timetable['teachers'][period] = periods[period]['assignments'][assignment].teacher
+                    break
+                if assignment == len(periods[period]['assignments']) - 1:
+                    timetable['days'][period] = periods[period]['day']
+                    timetable['periods'][period] = periods[period]['period']
         group_timetables.append(timetable)
 
     teacher_timetables = []
     for teacher in range(len(teachers)):
-        timetable = {'name': teachers[teacher].name, 'days': [], 'periods': [],
-                     'subjects': [], 'rooms': [], 'groups': []}
+        timetable = {'name': teachers[teacher].name, 'days': ["" for _ in range(40)],
+                     'periods': ["" for _ in range(40)], 'subjects': ["" for _ in range(40)],
+                     'rooms': ["" for _ in range(40)], 'groups': ["" for _ in range(40)]}
         for period in range(len(periods)):
-            for assignment in range(len(periods[period])):
+            for assignment in range(len(periods[period]['assignments'])):
                 if periods[period]['assignments'][assignment].teacher == teachers[teacher].name:
-                    timetable['days'].append(periods[period]['day'])
-                    timetable['periods'].append(periods[period]['period'])
-                    timetable['subjects'].append(periods[period]['assignments'][assignment].subject)
-                    timetable['rooms'].append(periods[period]['assignments'][assignment].room)
-                    timetable['groups'].append(periods[period]['assignments'][assignment].name)
+                    timetable['days'][period] = periods[period]['day']
+                    timetable['periods'][period] = periods[period]['period']
+                    timetable['subjects'][period] = periods[period]['assignments'][assignment].subject
+                    timetable['rooms'][period] = periods[period]['assignments'][assignment].room
+                    timetable['groups'][period] = periods[period]['assignments'][assignment].name
+                    break
+                if assignment == len(periods[period]['assignments']):
+                    timetable['days'][period] = periods[period]['day']
+                    timetable['periods'][period] = periods[period]['period']
         teacher_timetables.append(timetable)
 
     room_timetables = []
     for room in range(len(rooms)):
-        timetable = {'name': rooms[room].name, 'days': [], 'periods': [],
-                     'subjects': [], 'teachers': [], 'groups': []}
+        timetable = {'name': rooms[room].name, 'days': ["" for _ in range(40)],
+                     'periods': ["" for _ in range(40)], 'subjects': ["" for _ in range(40)],
+                     'teachers': ["" for _ in range(40)], 'groups': ["" for _ in range(40)]}
         for period in range(len(periods)):
-            for assignment in range(len(periods[period])):
+            for assignment in range(len(periods[period]['assignments'])):
                 if periods[period]['assignments'][assignment].room == rooms[room].name:
-                    timetable['days'].append(periods[period]['day'])
-                    timetable['periods'].append(periods[period]['period'])
-                    timetable['subjects'].append(periods[period]['assignments'][assignment].subject)
-                    timetable['teachers'].append(periods[period]['assignments'][assignment].teacher)
-                    timetable['groups'].append(periods[period]['assignments'][assignment].name)
+                    timetable['days'][period] = periods[period]['day']
+                    timetable['periods'][period] = periods[period]['period']
+                    timetable['subjects'][period] = periods[period]['assignments'][assignment].subject
+                    timetable['teachers'][period] = periods[period]['assignments'][assignment].teacher
+                    timetable['groups'][period] = periods[period]['assignments'][assignment].name
+                    break
+                if assignment == len(periods[period]['assignments']):
+                    timetable['days'][period] = periods[period]['day']
+                    timetable['periods'][period] = periods[period]['period']
         room_timetables.append(timetable)
 
     for timetable in range(len(group_timetables)):
@@ -82,9 +83,9 @@ def generateTimetable(timetable_datas, rooms, teachers, simple_groups):
 def sortGroupTimetable(timetable):
     counter = 0
     for day in time_windows.keys():
-        for period in range(len(time_windows['day'])):
+        for period in range(len(time_windows[day])):
             for assignment in range(len(timetable['periods'])):
-                if timetable['periods'][assignment] == time_windows['day'][period] and\
+                if timetable['periods'][assignment] == time_windows[day][period] and\
                    timetable['days'][assignment] == day:
                     tmp = {'day': timetable['days'][counter], 'period': timetable['periods'][counter],
                            'subject': timetable['subjects'][counter], 'room': timetable['rooms'][counter],
@@ -100,14 +101,15 @@ def sortGroupTimetable(timetable):
                     timetable['rooms'][assignment] = tmp['room']
                     timetable['teachers'][assignment] = tmp['teacher']
                     counter += 1
+                    break
 
 
 def sortTeacherTimetable(timetable):
     counter = 0
     for day in time_windows.keys():
-        for period in range(len(time_windows['day'])):
+        for period in range(len(time_windows[day])):
             for assignment in range(len(timetable['periods'])):
-                if timetable['periods'][assignment] == time_windows['day'][period] and\
+                if timetable['periods'][assignment] == time_windows[day][period] and\
                    timetable['days'][assignment] == day:
                     tmp = {'day': timetable['days'][counter], 'period': timetable['periods'][counter],
                            'subject': timetable['subjects'][counter], 'room': timetable['rooms'][counter],
@@ -123,14 +125,15 @@ def sortTeacherTimetable(timetable):
                     timetable['rooms'][assignment] = tmp['room']
                     timetable['groups'][assignment] = tmp['group']
                     counter += 1
+                    break
 
 
 def sortRoomTimetable(timetable):
     counter = 0
     for day in time_windows.keys():
-        for period in range(len(time_windows['day'])):
+        for period in range(len(time_windows[day])):
             for assignment in range(len(timetable['periods'])):
-                if timetable['periods'][assignment] == time_windows['day'][period] and \
+                if timetable['periods'][assignment] == time_windows[day][period] and \
                         timetable['days'][assignment] == day:
                     tmp = {'day': timetable['days'][counter], 'period': timetable['periods'][counter],
                            'subject': timetable['subjects'][counter], 'group': timetable['groups'][counter],
@@ -146,3 +149,5 @@ def sortRoomTimetable(timetable):
                     timetable['groups'][assignment] = tmp['group']
                     timetable['teachers'][assignment] = tmp['teacher']
                     counter += 1
+                    break
+
