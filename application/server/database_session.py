867974ac1e0792eded99fee5a76a2fb6f5b7a61c
Imre Piller <piller@iit.uni-miskolc.hu>
2021-04-27 22:56:04 +0200

    Some repository cleaning

--- application/server/database_session.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-from model import Base
-
-
-def createSession(engine):
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
--- application/server/subject.py
+++
@@ -1,5 +0,0 @@
-class Subject:
-    def __init__(self, name, type, weekly_periods):
-        self.name = name
-        self.type = type
-        self.weekly_periods = weekly_periods
