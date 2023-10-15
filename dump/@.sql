abaf0c836348f187b183dafb3a8d88ea2fa47dbf
molnarfi93 <55393260+molnarfi93@users.noreply.github.com>
2023-10-15 14:28:15 +0200

    Delete dump directory

--- dump/@.sql
+++
@@ -1,5 +0,0 @@
--- MySQLShell dump 1.0.1  Distrib Ver 8.0.22 for Win64 on x86_64 - for MySQL 8.0.22 (MySQL Community Server (GPL)), for Win64 (x86_64)
---
--- Host: localhost
--- ------------------------------------------------------
--- Server version	8.0.23
